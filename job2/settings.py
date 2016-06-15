2ddaaef9c4b12198065953d220f2999ca4bfc140
Helge Dzierzon <dochelge@gmail.com>
2016-06-15 20:10:30 +1200

    Update settings.py

--- job2/settings.py
+++ job2/settings.py
@@ -84,15 +84,6 @@ DATABASES = {
         'HOST': '52.63.108.83',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
     },    
-    'default2': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': 'coural',                      # Or path to database file if using sqlite3.
-            # The following settings are not used with sqlite3:
-            'USER': 'coural',
-            'PASSWORD': 'lmn9p5zktm&Ptu',
-            'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
-            'PORT': '',                      # Set to empty string for default.
-    },
     'test': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
